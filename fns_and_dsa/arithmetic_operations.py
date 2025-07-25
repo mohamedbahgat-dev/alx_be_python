5953c87a2e7c995d826c414f5593932763a27194
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-07-25 17:16:58 +0300

    create arithmetic function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,6 +4,8 @@ def perform_operation(num1, num2, operation):
             return num1 + num2
         case 'subtract':
             return num1 - num2
+        case 'multiply':
+            return num1 * num2
         case 'divide':
              if num2 == 0:
                  return "error, can't divide by zero"
