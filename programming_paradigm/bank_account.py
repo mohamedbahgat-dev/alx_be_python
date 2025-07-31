2a139efbc2398f52b27924a9940ed3870ac04ff3
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-07-31 10:07:14 +0300

    OOP

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,13 @@
 class BankAccount:
     def __init__(self, account_balance = 0):
-        self.account_balance = float(account_balance)
+        self.account_balance = account_balance
 
     def deposit(self, amount):
-        self.account_balance += float(amount)
+        self.account_balance += amount
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
-            self.account_balance -= float(amount)
+            self.account_balance -= amount
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}") 
