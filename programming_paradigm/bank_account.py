959295c9450c64f88cb5d630be7cbb07daf4c4c9
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-07-31 10:36:39 +0300

    OOP

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,6 +8,9 @@ class BankAccount:
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
+            return True
+        else:
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}") 
