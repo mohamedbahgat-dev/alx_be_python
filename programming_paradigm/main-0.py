b41288e410746ab82343949fbb44e03c8af111bb
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-07-31 10:21:20 +0300

    OOP

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -13,10 +13,10 @@ def main():
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
-        print(f"Deposited: ${amount:.2f}")
+        print(f"Deposited: ${amount:.3f}")
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.3f}")
         else:
             print("Insufficient funds.")
     elif command == "display":
